d2cca062e1ad02262e0b47ea3e7a095672be60ba
Anisha Zacharia <sarahgeorge125@gmail.com>
2017-03-23 12:55:38 -0700

    Added models for book, review, and user

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -4,23 +4,24 @@ from django.db import models
 from ..loginreg.models import User
 
 class ReviewManager(models.Manager):
-    def addReview(self,data):
-        pass
-        # user = User.objects.get(id=id)
-        # self.create(title=data['title'], author=data['author']), comment=data['review'], rating=data['rating'])
+    pass
+
+class BookManager(models.Manager):
+    pass
 
 class Book(models.Model):
     title = models.CharField(max_length = 250)
     author = models.CharField(max_length = 100)
+    creator = models.ForeignKey(User, related_name="books")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    objects = ReviewManager()
+    objects = BookManager()
 
 class Review(models.Model):
     rating = models.IntegerField()
     comment = models.CharField(max_length = 1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    book_id = models.ForeignKey(Book, related_name="book_reviews")
-    user_id = models.ForeignKey(User, related_name="users")
+    book = models.ForeignKey(Book, related_name="book_reviews")
+    user = models.ForeignKey(User, related_name="book_reviews")
     objects = ReviewManager()
--- apps/reviews/views.py
+++ apps/reviews/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
+from ..loginreg.models import User
 
 def index(request):
     return render(request, 'reviews/index.html')
 
 def add(request):
-     return render(request, 'reviews/add.html')
+    x = User.objects.get(id=request.session['user'])
+    y = Book.objects.create(title=request.POST['title'], author=request.POST['author'], creator = x)
+    Review.objects.create(rating= request.POST['rating'], comment=request.POST['comment'], user = x, book = y)
+    return render(request, 'reviews/add.html')
 
 def show(request):
      pass
